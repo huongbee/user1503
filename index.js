ccb847e65d73939771e0d35f8e3dc043fa449249
Huong <huongnguyenak96@gmail.com>
2019-04-24 19:06:25 +0700

    dbconnect

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+require('./lib/dbconnect');
 const express = require('express');
+const parser = require('body-parser').urlencoded({extended:false})
 const app = express()
 app.set('view engine','ejs')
+app.use(parser)
 
 app.get('/signup',(req,res)=>{
     res.render('signup');
 })
+app.post('/signup',(req,res)=>{
+    const {email, fullname, password, password_comfirmation} = req.body
+    res.send({email, fullname, password, password_comfirmation})
+});
 
 app.listen(3000,()=>{
     console.log('Connect to port 3000');
