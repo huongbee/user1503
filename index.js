e657b808e4c65acbea39de83b7fa375b0c8478d9
Huong <huongnguyenak96@gmail.com>
2019-04-26 18:07:55 +0700

    signup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./lib/dbconnect');
 const express = require('express');
 const parser = require('body-parser').urlencoded({extended:false})
+const {UserModel} = require('./models/User')
+const { hash, compare } = require('./lib/bcrypt');
 const app = express()
 app.set('view engine','ejs')
 app.use(parser)
@@ -10,8 +12,25 @@ app.get('/signup',(req,res)=>{
 })
 app.post('/signup',(req,res)=>{
     const {email, fullname, password, password_comfirmation} = req.body
-    res.send({email, fullname, password, password_comfirmation})
+    if(password != password_comfirmation)
+        return res.send({'error':'Password not match!'});
+    hash(password)
+    .then(passwordHash=>{
+        return UserModel.create({
+            email, fullname, password: passwordHash
+        })
+    })
+    .then((user)=>{
+        console.log(user)
+        res.redirect('/signin');
+    })
+    .catch(err=>{
+        res.send('error',err.message)
+    })
 });
+app.get('/signin',(req,res)=>{
+    res.render('signin')
+})
 
 app.listen(3000,()=>{
     console.log('Connect to port 3000');
