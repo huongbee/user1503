e95baf1fac6caac3953fc479af52aea1ed731bc5
Huong <huongnguyenak96@gmail.com>
2019-04-26 18:17:26 +0700

    add flash message

--- index.js
+++ index.js
@@ -3,17 +3,32 @@ const express = require('express');
 const parser = require('body-parser').urlencoded({extended:false})
 const {UserModel} = require('./models/User')
 const { hash, compare } = require('./lib/bcrypt');
+const flash = require('connect-flash'); 
+const session = require('express-session');
 const app = express()
 app.set('view engine','ejs')
 app.use(parser)
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}))
+app.use(flash()) 
+app.use(function (req, res, next) {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+})
 
 app.get('/signup',(req,res)=>{
     res.render('signup');
 })
 app.post('/signup',(req,res)=>{
     const {email, fullname, password, password_comfirmation} = req.body
-    if(password != password_comfirmation)
-        return res.send({'error':'Password not match!'});
+    if(password != password_comfirmation){
+        req.flash('error_msg','Password not match!');
+        res.redirect('/signup')
+    }
     hash(password)
     .then(passwordHash=>{
         return UserModel.create({
@@ -25,7 +40,9 @@ app.post('/signup',(req,res)=>{
         res.redirect('/signin');
     })
     .catch(err=>{
-        res.send('error',err.message)
+        // req.flash('error_msg',err.message);
+        req.flash('error_msg', 'Email existed!');
+        res.redirect('/signup')
     })
 });
 app.get('/signin',(req,res)=>{
